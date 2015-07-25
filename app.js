14a47909964911a9800d0f1812cb1ed546215aca
amirarafat1 <amir.arafat@jamberry.com>
2015-07-25 14:16:52 -0600

    updated my app.js to be more up to date with latest knockout practices

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
-
 (function(){
-	var carMaker = {
-		car: ko.observable(),
-		array: ko.observableArray(),
-		addCar: function () {
+	var carMakerVM = function() {
+		this.car = ko.observable(),
+		this.array = ko.observableArray(),
+		this.addCar = function () {
 			this.array.push(this.car());
 		}
 	};
-
-	carMaker.array.push('Car Makers:');
-	ko.applyBindings(carMaker);
-})();
+	ko.applyBindings(new carMakerVM());
+})();	

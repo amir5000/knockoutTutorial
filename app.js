d4f7bd3c6fce66136a5d62f862fb8a55d182d060
amirarafat1 <amir.arafat@jamberry.com>
2015-07-28 16:38:53 -0600

    added a counter of every submit, and also disabled button when nothing is entered and cleared out the input field on submit.

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 (function(){
 	var carMakerVM = function() {
-		this.car = ko.observable(),
-		this.array = ko.observableArray(),
+		this.numberOfClicks = ko.observable(0);
+		this.car = ko.observable();
+		this.array = ko.observableArray();
 		this.addCar = function () {
 			this.array.push(this.car());
+			this.numberOfClicks(this.numberOfClicks() + 1);
+			this.car("");
 		}
 	};
 	ko.applyBindings(new carMakerVM());
-})();	
+})();
